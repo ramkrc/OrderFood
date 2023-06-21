b1d1ac74c54fc31c916c3e60ffc68e70a3d16d75
Ramazan <ramkrc11@gmail.com>
2023-06-21 16:59:02 -0700

    Initial Commit

---
+++ OrderFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderFood
+//
+//  Created by Ramazan on 6/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
